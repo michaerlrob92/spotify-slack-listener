9e058c06f6afe9bb21951699966a1f41d05beb38
Michael Robinson <mike.william.robinson@gmail.com>
2020-03-18 20:48:31 +0000

    Build script self-contained runtime for dotnet

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DOTNET_ROOT=/opt/buildhome/.dotnet
 ./dotnet-install.sh --version 3.1.200
-dotnet publish "SpotifySlackListener/SpotifySlackListener.csproj" -c Release -o "published"
+dotnet publish "SpotifySlackListener/SpotifySlackListener.csproj" -c Release -o "published" --runtime "linux-x64" --framework netcoreapp3.1
